d49cd7cfaa3d8d47a8c9302ca866ab4a9f616813
jobayedislam <islam.mjobayed@gmail.com>
2026-05-03 21:55:41 +0600

    al: add bubble sort algorithm for numbers

---
+++ JavaScript/Algorithms/bubble-sort.js
@@ -0,0 +1,13 @@
+const arr = [4, 2, 7, 1, 9, 3, 1, 6];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+
+console.log(arr);
