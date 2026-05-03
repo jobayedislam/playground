43e60db4a267338179edae54ec20d80fb1cb5474
jobayedislam <islam.mjobayed@gmail.com>
2026-05-04 00:00:30 +0600

    refactor: move bubble sort to functional approach

--- JavaScript/Algorithms/bubble-sort.js
+++ JavaScript/Algorithms/bubble-sort.js
@@ -1,13 +1,19 @@
-const arr = [4, 2, 7.3, 7.1, 1, 9, 3, 1, 6];
+const mainArr = [4, 2, 7.3, 7.1, 1, 9, 3, 1, 6];
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length - i; j++) {
-    if (arr[j] > arr[j + 1]) {
-      let temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
+const bubbleSort = (arr) => {
+  let newArr = arr;
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr.length - i; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        let temp = newArr[j];
+        newArr[j] = newArr[j + 1];
+        newArr[j + 1] = temp;
+      }
     }
   }
-}
 
-console.log(arr);
+  return newArr;
+};
+
+const result = bubbleSort(mainArr);
+console.log(result);
