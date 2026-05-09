97b67781ff3c75f43af949587171d4e4d49f136e
jobayedislam <islam.mjobayed@gmail.com>
2026-05-10 05:28:09 +0600

    others: implement an idea to delete item from an array of objects
    With the `forEach()` method, check for a specific key to get it's index. Then use `array.splice()` to remove exactly one element.

---
+++ JavaScript/Others/delete-item-from-array-of-objects.js
@@ -0,0 +1,45 @@
+const mainArray = [
+  {
+    id: "675284ea-41bf-47e2-a527-16fef147eaaa",
+    title: "Task 1",
+    completed: false,
+    timestamp: "1778368485727",
+  },
+  {
+    id: "8e541ffb-45c6-464e-8653-517a34bacc57",
+    title: "Task 2",
+    completed: false,
+    timestamp: "1778368485727",
+  },
+  {
+    id: "8c89029c-5dbe-434f-ac0e-9c940828331e",
+    title: "Task 3",
+    completed: false,
+    timestamp: "1778368485727",
+  },
+  {
+    id: "616b0d88-e3ee-44fd-9b8d-4be03bfbd41f",
+    title: "Task 4",
+    completed: false,
+    timestamp: "1778368485727",
+  },
+  {
+    id: "9ea8aa52-f59b-46fd-b76c-52a567b58619",
+    title: "Task 5",
+    completed: false,
+    timestamp: "1778368485727",
+  },
+];
+
+let taskIndex;
+mainArray.forEach((item) => {
+  if (item.id === "9ea8aa52-f59b-46fd-b76c-52a567b58619") {
+    taskIndex = mainArray.indexOf(item);
+  }
+});
+
+if (taskIndex && taskIndex >= 0) {
+  mainArray.splice(taskIndex, 1);
+}
+
+console.log(mainArray);
