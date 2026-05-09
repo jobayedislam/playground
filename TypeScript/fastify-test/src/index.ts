1b7ea9f044bf35bcacbdbcb75f0267bf75e3945c
jobayedislam <islam.mjobayed@gmail.com>
2026-05-09 07:37:03 +0600

    feat(root): initialize fastify server with top level await

--- TypeScript/fastify-test/src/index.ts
+++ TypeScript/fastify-test/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
