d43bed1c2fc5c1beb9cdfd88cbb4a7c530b529b2
jobayedislam <islam.mjobayed@gmail.com>
2026-05-09 15:52:08 +0600

    feat(tasks): implement in memory array

--- TypeScript/fastify/fastify-crud/src/index.ts
+++ TypeScript/fastify/fastify-crud/src/index.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
+import { Task } from "./types.js";
 
 const server = fastify({ logger: true });
+const tasks: Task[] = [];
 
 server.get("/", async (req, res) => {
   res.code(200).send({ message: "OK" });
