5d36c6c935e7b290a9d74cef62447d44c65cdf18
jobayedislam <islam.mjobayed@gmail.com>
2026-05-09 15:23:20 +0600

    feat(root): initialize server and add root GET route

---
+++ TypeScript/fastify/fastify-crud/src/index.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get("/", async (req, res) => {
+  res.code(200).send({ message: "OK" });
+});
+
+try {
+  server.listen({ port: 3000 });
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+}
