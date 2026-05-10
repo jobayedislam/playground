178e2fd13e6f8531580dc484ff3cbe90c9269685
jobayedislam <islam.mjobayed@gmail.com>
2026-05-11 05:18:51 +0600

    feat(root): add `userGroup` to `/user` prefix

--- TypeScript/fastify/fastify-nested-folder-routing/src/index.ts
+++ TypeScript/fastify/fastify-nested-folder-routing/src/index.ts
@@ -1,10 +1,13 @@
 import fastify from "fastify";
+import { userGroup } from "./routes/user";
 
 const server = fastify({ logger: true });
 
 server.get("/", async (req, res) => {
   res.code(200).send({ status: "OK" });
 });
+
+server.register(userGroup, { prefix: "/user" });
 
 try {
   server.listen({ port: 3000 });
