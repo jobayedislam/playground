7b7a7d06fe405a2b3e40c500d249bda8c728209e
jobayedislam <islam.mjobayed@gmail.com>
2026-05-11 04:31:32 +0600

    feat(root): implement status check `GET` request on root path

--- TypeScript/fastify/fastify-multi-file-routing/src/index.ts
+++ TypeScript/fastify/fastify-multi-file-routing/src/index.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 
 const server = fastify({ logger: true });
 
+server.get("/", async (req, res) => {
+  res.code(200).send({ status: "OK" });
+});
+
 try {
   server.listen({ port: 3000 });
 } catch (err) {
