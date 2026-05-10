e15d6a37ff01f33041d20b14544aae679ea987f6
jobayedislam <islam.mjobayed@gmail.com>
2026-05-11 05:24:18 +0600

    feat(user): register `usersRoutes` to `/user/users` prefix

--- TypeScript/fastify/fastify-nested-folder-routing/src/routes/user/index.ts
+++ TypeScript/fastify/fastify-nested-folder-routing/src/routes/user/index.ts
@@ -1,6 +1,8 @@
 import fastify, { FastifyInstance } from "fastify";
 import { adminRoutes } from "./admin/adminRoutes";
+import { usersRoute } from "./users/usersRoutes";
 
 export const userGroup = async (server: FastifyInstance) => {
   server.register(adminRoutes, { prefix: "/admin" });
+  server.register(usersRoute, { prefix: "/users" });
 };
