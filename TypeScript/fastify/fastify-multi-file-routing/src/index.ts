34761e270542b2937c6364fddf7cbdd62da89b27
jobayedislam <islam.mjobayed@gmail.com>
2026-05-11 04:30:46 +0600

    feat(root): initialize fastify server on port 3000

--- TypeScript/fastify/fastify-multi-file-routing/src/index.ts
+++ TypeScript/fastify/fastify-multi-file-routing/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+try {
+  server.listen({ port: 3000 });
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+}
