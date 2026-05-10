f5704bc08d4acd34c85d5167c6cdbe5ba153bf09
jobayedislam <islam.mjobayed@gmail.com>
2026-05-11 04:40:52 +0600

    feat(root): add `todoRoutes` to `/todos` prefix with `fastify.register()`

--- TypeScript/fastify/fastify-multi-file-routing/src/index.ts
+++ TypeScript/fastify/fastify-multi-file-routing/src/index.ts
@@ -1,10 +1,13 @@
 import fastify from "fastify";
+import { todoRoutes } from "./routes/todoRoutes";
 
 const server = fastify({ logger: true });
 
 server.get("/", async (req, res) => {
   res.code(200).send({ status: "OK" });
 });
+
+server.register(todoRoutes, { prefix: "/todos" });
 
 try {
   server.listen({ port: 3000 });
