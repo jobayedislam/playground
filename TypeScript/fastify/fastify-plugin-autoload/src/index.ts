cc97e4a2ece2bb6b6ee760c4f144ba4a4a9e94a4
jobayedislam <islam.mjobayed@gmail.com>
2026-05-11 21:17:22 +0600

    feat(autoload): implement `@fastify/autoload` plugin
    This configuration is taken directly from the `@fastify/autoload` project's GitHub.

--- TypeScript/fastify/fastify-plugin-autoload/src/index.ts
+++ TypeScript/fastify/fastify-plugin-autoload/src/index.ts
@@ -1,6 +1,15 @@
 import fastify from "fastify";
+import autoload from "@fastify/autoload";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const server = fastify({ logger: true });
+
+server.register(autoload, {
+  dir: join(__dirname, "routes"),
+});
 
 server.get("/", async (req, res) => {
   res.code(200).send({ status: "OK" });
