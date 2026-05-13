8d92c790cd9056ad61259938ca6818dd25fce34e
jobayedislam <islam.mjobayed@gmail.com>
2026-05-14 05:01:46 +0600

    feat: move seed logic to `src/seed.ts`

--- TypeScript/databases/node-sqlite/sqlite-seed-file/src/index.ts
+++ TypeScript/databases/node-sqlite/sqlite-seed-file/src/index.ts
@@ -1,25 +1,3 @@
-import { DatabaseSync } from "node:sqlite";
-import * as fs from "node:fs";
-import { data } from "./seed.js";
+import { db } from "./seed.js";
 
-const dbFile = "./data.db";
-
-if (fs.existsSync(dbFile)) {
-  fs.unlinkSync(dbFile);
-  console.log("Database Deleted!");
-}
-
-const db = new DatabaseSync(dbFile);
-
-db.exec(`
-    CREATE TABLE users(
-        key INTEGER PRIMARY KEY,
-        name TEXT
-    ) STRICT
-`);
-
-const insert = db.prepare("INSERT INTO users (name) VALUES (?)");
-
-data.forEach((item) => {
-  insert.run(item.name);
-});
+console.log(db);
--- TypeScript/databases/node-sqlite/sqlite-seed-file/src/seed.ts
+++ TypeScript/databases/node-sqlite/sqlite-seed-file/src/seed.ts
@@ -1,8 +1,29 @@
+import { DatabaseSync } from "node:sqlite";
+import * as fs from "node:fs";
+
+const dbFile = "./data.db";
+
+if (fs.existsSync(dbFile)) {
+  fs.unlinkSync(dbFile);
+  console.log("Database Deleted!");
+}
+
+export const db = new DatabaseSync(dbFile);
+
+db.exec(`
+    CREATE TABLE users(
+        key INTEGER PRIMARY KEY,
+        name TEXT
+    ) STRICT
+`);
+
+const insert = db.prepare("INSERT INTO users (name) VALUES (?)");
+
 interface Users {
   name: string;
 }
 
-export const data: Users[] = [
+const data: Users[] = [
   { name: "John Doe" },
   { name: "Jahn Doe" },
   { name: "Alice Hamilton" },
@@ -12,3 +33,7 @@ export const data: Users[] = [
   { name: "রহিম" },
   { name: "" },
 ];
+
+data.forEach((item) => {
+  insert.run(item.name);
+});
