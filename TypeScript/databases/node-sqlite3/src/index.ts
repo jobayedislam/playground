8bffa4b2d57afa2b6884ebca2ec6f69e8ce890d2
jobayedislam <islam.mjobayed@gmail.com>
2026-05-13 07:19:47 +0600

    docs: add reasons to use uppercase for SQL query keywords

--- TypeScript/databases/node-sqlite3/src/index.ts
+++ TypeScript/databases/node-sqlite3/src/index.ts
@@ -13,5 +13,12 @@ const insert = db.prepare("INSERT INTO users (name) VALUES (?)");
 insert.run("John Doe");
 insert.run("Jane Doe");
 
+// NOTE: The case for using uppercase for SQL query keywords
+// 1. SQL style guide recommends it.
+// 2. It offers readability without highlight.
+// 3. It provides a distinction between keywords and constants
+// 4. During 1970s, when SQL was born, many keyboards apparently did not support
+//    lowercase letters. (That's news to me). So, languages like COBOL and
+//    FORTRAN also use uppercase by default.
 const query = db.prepare("select * from users");
 console.log(query.all());
