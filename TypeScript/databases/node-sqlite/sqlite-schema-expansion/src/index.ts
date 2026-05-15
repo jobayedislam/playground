2ee7bf8ef1f59219fb9db2440bdc8332c62b5917
jobayedislam <islam.mjobayed@gmail.com>
2026-05-15 17:01:44 +0600

    feat: create a database file

--- TypeScript/databases/node-sqlite/sqlite-schema-expansion/src/index.ts
+++ TypeScript/databases/node-sqlite/sqlite-schema-expansion/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { DatabaseSync } from "node:sqlite";
+import * as fs from "node:fs";
+
+const dbFile = "./data.db";
+
+if (fs.existsSync(dbFile)) {
+  fs.unlinkSync(dbFile);
+  console.log("Database deleted");
+}
+
+export const db = new DatabaseSync(dbFile);
+
+console.log(db);
