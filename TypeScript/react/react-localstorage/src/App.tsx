d2d6982b1ad6aa6f3beb8c403d7161e1b5c3a7de
jobayedislam <islam.mjobayed@gmail.com>
2026-05-16 16:01:22 +0600

    feat: remove useRef section
    I learned useRef is good for simple form input, but for more complex? tasks like validation or clearing the input field, it can be more complicated than just using useState.

--- TypeScript/react/react-localstorage/src/App.tsx
+++ TypeScript/react/react-localstorage/src/App.tsx
@@ -1,15 +1,10 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [text, setText] = useState<string>("");
-  const textRef = useRef<HTMLInputElement>(null);
 
   const handleClick = () => {
     console.log(text);
-  };
-
-  const handleRefClick = () => {
-    console.log(textRef.current?.value);
   };
 
   return (
@@ -17,9 +12,6 @@ function App() {
       <h1>LocalStorage test</h1>
       <input type="text" onChange={(e) => setText(e.target.value)} />
       <button onClick={handleClick}>Show Text</button>
-      <hr />
-      <input type="text" ref={textRef} />
-      <button onClick={handleRefClick}>Show Text</button>
     </>
   );
 }
