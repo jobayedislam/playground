5e38c67d40dd6c70e6b9aeee3965ab3985f206d3
jobayedislam <islam.mjobayed@gmail.com>
2026-05-16 15:55:01 +0600

    feat: create show button and click handler function

--- TypeScript/react/react-localstorage/src/App.tsx
+++ TypeScript/react/react-localstorage/src/App.tsx
@@ -3,10 +3,15 @@ import { useState } from "react";
 function App() {
   const [text, setText] = useState<string>("");
 
+  const handleClick = () => {
+    console.log(text);
+  };
+
   return (
     <>
       <h1>LocalStorage test</h1>
       <input type="text" onChange={(e) => setText(e.target.value)} />
+      <button onClick={handleClick}>Show Text</button>
     </>
   );
 }
