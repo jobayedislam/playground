059dfe614212626f7bfec069d4a1d328e2daaa3c
jobayedislam <islam.mjobayed@gmail.com>
2026-05-16 15:53:32 +0600

    feat: add input box and capture text

--- TypeScript/react/react-localstorage/src/App.tsx
+++ TypeScript/react/react-localstorage/src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState<string>("");
+
   return (
     <>
       <h1>LocalStorage test</h1>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
     </>
   );
 }
