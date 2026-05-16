dc9689dca1ae554e52596c6dd946e71937058886
jobayedislam <islam.mjobayed@gmail.com>
2026-05-16 15:59:03 +0600

    feat: implement useRef on a second set of `input` and `button`

--- TypeScript/react/react-localstorage/src/App.tsx
+++ TypeScript/react/react-localstorage/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   const [text, setText] = useState<string>("");
+  const textRef = useRef<HTMLInputElement>(null);
 
   const handleClick = () => {
     console.log(text);
+  };
+
+  const handleRefClick = () => {
+    console.log(textRef.current?.value);
   };
 
   return (
@@ -12,6 +17,9 @@ function App() {
       <h1>LocalStorage test</h1>
       <input type="text" onChange={(e) => setText(e.target.value)} />
       <button onClick={handleClick}>Show Text</button>
+      <hr />
+      <input type="text" ref={textRef} />
+      <button onClick={handleRefClick}>Show Text</button>
     </>
   );
 }
