a6f7c9ce0823107d7f2b92c85bb014edc9dcc585
jobayedislam <islam.mjobayed@gmail.com>
2026-05-17 04:28:24 +0600

    feat: add padding below the books toggle button

--- TypeScript/react/react-data-generator-simple/src/App.tsx
+++ TypeScript/react/react-data-generator-simple/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <h1>Simple Data Generator example</h1>
-      <button onClick={handleToggleBooks}>
+      <button onClick={handleToggleBooks} className="toggle-books-btn">
         {areBooksShown ? "Hide Books" : "Show Books"}
       </button>
       <div className="container">
