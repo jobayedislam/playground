e5f98f119dc483043707b1bdcf1c2c9abfbd6c7b
jobayedislam <islam.mjobayed@gmail.com>
2026-05-17 03:44:14 +0600

    feat: implement book data generator

--- TypeScript/react/react-data-generator-simple/src/data/dataGenerator.ts
+++ TypeScript/react/react-data-generator-simple/src/data/dataGenerator.ts
@@ -5,8 +5,19 @@ interface Book {
   author: string;
 }
 
-const generateData = (): string => {
-  return "Hello World";
+const generateData = (): Book[] => {
+  const books = [];
+  for (let i = 0; i < 10; i++) {
+    const book = {
+      id: i + 1,
+      name: `Book NO: ${i + 1}`,
+      isbn: Math.floor(Math.random() * 9000000000000) + 1000000000000,
+      author: `Author NO: ${Math.floor(Math.random() * 10) + 1}`,
+    };
+    books.push(book);
+  }
+
+  return books;
 };
 
 export default generateData;
