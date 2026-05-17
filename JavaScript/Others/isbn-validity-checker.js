f9e04f04af5008cb194e48442bafc32e9f813a16
jobayedislam <islam.mjobayed@gmail.com>
2026-05-18 04:44:30 +0600

    feat: add some test input values
    I know it's extremely inefficient this way

---
+++ JavaScript/Others/isbn-validity-checker.js
@@ -0,0 +1,11 @@
+const input = "978-0-553-59371-6"; // 13-digit (valid)
+// const input = "9780553593716"; // 13-digit (valid)
+// const input = "978-0-553-59371-7"; // 13-digit (invalid)
+// const input = "9780553593717"; // 13-digit (invalid)
+
+// const input = "0-316-76948-7"; // 10-digit (valid)
+// const input = "0316769487"; // 10-digit (valid)
+// const input = "0-12-515430-X"; // 10-digit (invalid)
+// const input = "012515430X"; // 10-digit (invalid)
+
+console.log(input);
