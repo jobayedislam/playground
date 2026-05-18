1d2b6638a08eb8e8047e43a1a36f8aa8bd92f989
jobayedislam <islam.mjobayed@gmail.com>
2026-05-19 04:08:25 +0600

    feat: initialize a simple fastify server

--- TypeScript/fastify/fastify-sqlite/src/index.ts
+++ TypeScript/fastify/fastify-sqlite/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get("/", async (req, res) => {
+  res.code(200).send({ message: "OK" });
+});
+
+try {
+  server.listen({ port: 3000 });
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+}
