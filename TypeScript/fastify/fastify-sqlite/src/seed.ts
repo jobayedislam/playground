ee55a32dae8deffe8e2c87e131d2d4c14c00dbb5
jobayedislam <islam.mjobayed@gmail.com>
2026-05-18 20:39:44 +0600

    feat(seed): create a database file after deleting old one

--- TypeScript/fastify/fastify-sqlite/src/seed.ts
+++ TypeScript/fastify/fastify-sqlite/src/seed.ts
@@ -1,1 +1,11 @@
-console.log("From seed");
+import { DatabaseSync } from "node:sqlite";
+import * as fs from "node:fs";
+
+const dbFile = "./data.db";
+
+if (fs.existsSync(dbFile)) {
+  fs.unlinkSync(dbFile);
+  console.log("Database Deleted!");
+}
+
+const db = new DatabaseSync(dbFile);
