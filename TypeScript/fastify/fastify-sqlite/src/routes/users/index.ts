967630df4a7ef35970365c7a3207c41fd6194b59
jobayedislam <islam.mjobayed@gmail.com>
2026-05-19 04:57:07 +0600

    feat(users): implement `POST` request to query email
    What this commit does:
    - create a new instance of the database.
    - define a type for request body.
    - implement the `POST` request function.
    - query the database for email matches.
    - respond with the email.

--- TypeScript/fastify/fastify-sqlite/src/routes/users/index.ts
+++ TypeScript/fastify/fastify-sqlite/src/routes/users/index.ts
@@ -1,8 +1,20 @@
 import { FastifyInstance } from "fastify";
+import { DatabaseSync } from "node:sqlite";
+const db = new DatabaseSync("./data.db");
 
 const usersRoute = async (server: FastifyInstance) => {
   server.get("/", async (req, res) => {
     res.code(200).send({ message: "Successfully reached usersRoute" });
+  });
+
+  type GetUserBody = { email: string };
+  server.post<{ Body: GetUserBody }>("/list", async (req, res) => {
+    const { email } = req.body;
+
+    const query = db.prepare("SELECT * FROM users WHERE email = (?)");
+    const result = query.get(email);
+
+    res.code(201).send(result);
   });
 };
 
