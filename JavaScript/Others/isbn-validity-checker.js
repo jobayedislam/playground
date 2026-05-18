1161021a929ade6bd904d324e6d76c19e34984b6
jobayedislam <islam.mjobayed@gmail.com>
2026-05-18 13:34:00 +0600

    feat: use the correct function depending on the input length

--- JavaScript/Others/isbn-validity-checker.js
+++ JavaScript/Others/isbn-validity-checker.js
@@ -6,7 +6,7 @@ const input = "978-0-553-59371-6"; // 13-digit (valid)
 // const input = "0-316-76948-7"; // 10-digit (valid)
 // const input = "0316769487"; // 10-digit (valid)
 // const input = "0-12-515430-X"; // 10-digit (invalid)
-const input = "012515430X"; // 10-digit (invalid)
+// const input = "012515430X"; // 10-digit (invalid)
 
 const sanitizeIsbn = (isbn) => {
   const sanitized = isbn
@@ -44,10 +44,10 @@ const validateThirteenDigit = (isbn) => {
 const checkIsbn = (isbn) => {
   const sanitizedIsbn = sanitizeIsbn(isbn);
 
-  if (sanitizedIsbn.length !== 10 && sanitizedIsbn.length !== 13)
-    return "Invalid input length";
-
-  return sanitizedIsbn;
+  if (sanitizedIsbn.length === 10) return validateTenDigit(sanitizedIsbn);
+  else if (sanitizedIsbn.length === 13)
+    return validateThirteenDigit(sanitizedIsbn);
+  else return "Invalid input length.";
 };
 
 console.log(checkIsbn(input));
