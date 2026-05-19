771baf79bf0366e7fa9c598991477b85d2030879
jobayedislam <islam.mjobayed@gmail.com>
2026-05-19 20:23:50 +0600

    refactor!: remove `POST` request from `/users/list` route

--- TypeScript/fastify/fastify-sqlite/src/routes/users/index.ts
+++ TypeScript/fastify/fastify-sqlite/src/routes/users/index.ts
@@ -7,20 +7,6 @@ const usersRoute = async (server: FastifyInstance) => {
     res.code(200).send({ message: "Successfully reached usersRoute" });
   });
 
-  type GetUserBody = { email: string };
-  server.post<{ Body: GetUserBody }>("/list", async (req, res) => {
-    const { email } = req.body;
-
-    const query = db.prepare("SELECT * FROM users WHERE email = (?)");
-    const result = query.get(email);
-
-    if (result) {
-      res.code(200).send(result);
-    } else {
-      res.code(404).send({ message: "Not found" });
-    }
-  });
-
   type GetUserParams = { id: number };
   server.get<{ Params: GetUserParams }>("/list/:id", async (req, res) => {
     const { id } = req.params;
